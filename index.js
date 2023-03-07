c16b3b4590e59ebac01839c2fe8af4f9853ec404
unknown <olloyd321@gmail.com>
2023-03-07 13:02:30 -0500

    inquirer v8

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const inquirer = require('inquirer'); // inquirer package 
+const fs = require('fs'); // file system package
+
 console.log("welcome to the SVG Logo Generator");
+function init(){
+    inquirer.prompt(user.prompts.js).then(() => {
+        
+    });
+};
