34c30d7c35b3582f2e34d05cec938262773f403b
unknown <olloyd321@gmail.com>
2023-03-09 17:53:20 -0500

    added js functionality for generating shapes and writing svg file

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 const inquirer = require('inquirer'); // inquirer package 
 const fs = require('fs'); // file system package
 const generateUserPrompts = require('./lib/user.prompts');
+const { Circle, Triangle, Square } = require('./lib/generate.shapes.js');
 
-
-function init(){
-    inquirer.prompt(generateUserPrompts);
+function init() {
+    inquirer.prompt(generateUserPrompts).then(response => {
+        console.log(response)
+        let shape;
+        switch (response.shape) {
+            case "circle":
+                shape = new Circle()
+                shape.setColor(response.shapeColor)
+                break
+            case "triangle":
+                shape = new Triangle()
+                shape.setColor(response.shapeColor)
+                break
+            default:
+                shape = new Square()
+                shape.setColor(response.shapeColor)
+        }
+        // if(response.shape = 'circle'){
+        //     shape = new Circle()
+        //     shape.setColor(response.shapeColor)
+        // }
+        // else if(response.shape = 'triangle'){
+        //     shape = new Triangle()
+        //     shape.setColor(response.shapeColor)
+        // }
+        // else if(response.shape = 'square'){
+        //     shape = new Square()
+        //     shape.setColor(response.shapeColor)
+        // }
+        console.log(shape);
+        return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        ${shape.render()}
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${response.textColor}">${response.nameOfLogo}</text>
+        </svg>
+        `
+    }).then(data => {
+        console.log(data);
+        fs.writeFileSync("logo.svg", data)
+    })
 };
 
+init()
