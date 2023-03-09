4bbc02e288da8f4f677ff4553fb12220ae40dcfc
unknown <olloyd321@gmail.com>
2023-03-09 18:08:55 -0500

    previous commit *lines 23-34* was not functioning as expected - current commit resolves issue and comments out lines 10-22 (lines 10-22 were a workaround for initial issues on lines 23-34) **lines 10-22 commented out for reference but will be removed before final push

--- index.js
+++ index.js
@@ -7,31 +7,31 @@ function init() {
     inquirer.prompt(generateUserPrompts).then(response => {
         console.log(response)
         let shape;
-        switch (response.shape) {
-            case "circle":
-                shape = new Circle()
-                shape.setColor(response.shapeColor)
-                break
-            case "triangle":
-                shape = new Triangle()
-                shape.setColor(response.shapeColor)
-                break
-            default:
-                shape = new Square()
-                shape.setColor(response.shapeColor)
+        // switch (response.shape) {
+        //     case "circle":
+        //         shape = new Circle()
+        //         shape.setColor(response.shapeColor)
+        //         break
+        //     case "triangle":
+        //         shape = new Triangle()
+        //         shape.setColor(response.shapeColor)
+        //         break
+        //     default:
+        //         shape = new Square()
+        //         shape.setColor(response.shapeColor)
+        // }
+        if (response.shape === 'circle'){
+            shape = new Circle()
+            shape.setColor(response.shapeColor)
         }
-        // if(response.shape = 'circle'){
-        //     shape = new Circle()
-        //     shape.setColor(response.shapeColor)
-        // }
-        // else if(response.shape = 'triangle'){
-        //     shape = new Triangle()
-        //     shape.setColor(response.shapeColor)
-        // }
-        // else if(response.shape = 'square'){
-        //     shape = new Square()
-        //     shape.setColor(response.shapeColor)
-        // }
+        else if(response.shape === 'triangle'){
+            shape = new Triangle()
+            shape.setColor(response.shapeColor)
+        }
+        else {
+            shape = new Square()
+            shape.setColor(response.shapeColor)
+        }
         console.log(shape);
         return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
         ${shape.render()}
